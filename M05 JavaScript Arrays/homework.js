197801dcde13854cb5be550c62a874484dec18f2
EDWARDROAG <edwardroag@gmail.com>
2023-04-01 12:38:25 -0500

    finalizacion test 5

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -186,18 +186,45 @@ function mesesDelAño(array) {
    // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
    // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
    // Tu código:
+   let MesesPedidos = [];
+   for(let i = 0 ; i < array.length; i++){
+      if(array[i]==="Enero" || array[i]=== "Marzo"|| array[i]==="Noviembre"){
+         MesesPedidos.push(array[i]);
+      }
+   }
+      if(MesesPedidos.length === 3){
+         return MesesPedidos;
+      }else{
+         return "No se encontraron los meses pedidos";
+
+      }
+
 }
 
 function tablaDelSeis() {
    // Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
    // La función devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
    // Tu código:
+   let TablaSeis = [];
+   for(let i = 0; i <= 10; i++){
+      TablaSeis.push(6*i);
+   }
+   return TablaSeis;
 }
 
 function mayorACien(array) {
    // La función recibe un arreglo con enteros entre 0 y 200.
    // Recorrerlo y retornar un arreglo con todos los valores mayores a 100 (no incluye el 100).
    // Tu código:
+   let MayorQue =[];
+
+   for( let i  = 0; i < array.length; i++) {
+      if(array[i] > 100){
+         MayorQue.push(array[i]);      
+     }   
+   }
+   return MayorQue;
+
 }
 
 /* ----------------------------------------------------------------------------------
@@ -211,7 +238,22 @@ function breakStatement(num) {
    // la ejecución y retornar el string: "Se interrumpió la ejecución".
    // [PISTA]: utiliza el statement 'break'.
    // Tu código:
+
+   let newArray = [];
+  let sum = num;
+  for (let i = 0; i < 10; i++) {
+    sum += 2;
+    if (sum === i) {
+      return "Se interrumpió la ejecución";
+      break;
+    }
+    newArray.push(sum);
+  }
+  return newArray;
 }
+
+
+
 
 function continueStatement(num) {
    // Iterar en un bucle aumentando en 2 el número recibido hasta un límite de 10 veces.
@@ -220,7 +262,20 @@ function continueStatement(num) {
    // se continua con la siguiente iteración.
    // [PISTA]: utiliza el statement 'continue'.
    // Tu código:
+   
+   let newArray = [];
+  let sum = num;
+  for (let i = 0; i < 10; i++) {
+    
+    if (i === 5) {
+      continue;
+    }
+    sum += 2;
+    newArray.push(sum);
+  }
+  return newArray;
 }
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
